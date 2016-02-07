bb6c4c08c5654437f44d3bc58dfc24571205c66f
mura8boo <phpwon-fc2@yahoo.co.jp>
2016-02-08 08:52:00 +0900

    method_missing を利用したメソッドを追加

--- xml_pars.rb
+++ xml_pars.rb
@@ -23,17 +23,37 @@ class XMLParser
 end
 
 class People < XMLParser
-  def read_people(xpath)
-    by_xpath(xpath) do |node| 
-      puts node.get('id')
-      puts node.at_xpath('name').text
+  def read_people(xpath, block)
+    by_xpath(xpath) { |node| block.call node }
+  end
+
+  def method_missing(method, *args)
+    unless method.to_s.start_with? 'xpath_'
+      super
     end
+    xpath = to_xpath(method)
+    by_xpath(xpath) { |node| args[0].call node }
+  end
+
+  private
+  def to_xpath(method)
+    elms = method.to_s.split('_')
+    elms[1..-1].join('/')
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   fname = 'people.xml'
   p = People.open fname
+
+  check = Proc.new do |node|
+    puts " - id:#{node.get('id')}, name:#{node.at_xpath('name').text}"
+  end
+
+  puts '1. xpath を利用'
   xpath = 'peoples/people/person'
-  p.read_node xpath
+  p.read_people(xpath, check)
+
+  puts '2. メソッド名からxpath を生成してパース'
+  xpath = p.xpath_peoples_people_person check
 end
